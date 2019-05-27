af89680de26a95580bcf089d5620ebe760d05faf
arshdeep1109 <50379194+arshdeep1109@users.noreply.github.com>
2019-05-27 09:31:41 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rehan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            String num1 = Console.ReadLine();
+            Console.WriteLine("enter a number");
+            String num2 = Console.ReadLine();
+            int sum = Convert.ToInt32(num1) + Convert.ToInt32(num2);
+            Console.WriteLine("sum is "+sum);
+        }
+    }
+}
---
+++ practice assign/practice assign/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rehan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] red = new int[5];
+
+            Console.WriteLine("enter some number");
+            Console.WriteLine("enter fini number");
+            string input = "";
+            int product = 1;
+            int i = 0;
+            while(true)
+            {
+                input = Console.ReadLine();
+                if (input.ToLower().Equals("fini")) 
+                { break; }
+                red[i++] = Convert.ToInt32(input);
+                if (i == red.Length) ;
+                {
+                    Console.WriteLine("out of erray");
+
+                    break;
+                }
+
+            }
+            Console.WriteLine("out of erray{0}",product);
+
+        }
+
+    }
+}
